6ce24b6eea313aa07c50e77e635e0ff64075d773
Sebas Girón <sebasgiron@gmail.com>
2021-11-07 13:45:55 +0100

    preparar http_server para futuros usos

--- quick-notes/http_server.js
+++ quick-notes/http_server.js
@@ -1,34 +1,45 @@
 const http = require('http'); 
 const fs = require('fs'); 
-
-
 const SRV_PUERTO = 8081; 
-const SRV_HOST = 'localhost'; 
 
-const entregar_archivo = function(archivo, res) {
-	var archivo = fs.createReadStream(archivo); 
-	res.writeHead(200, {'Content-Type': 'text/html'}); 
-	archivo.pipe(res); 
+/* Funciones de respuesta ************************************************** */
+const dummyResponse = function(res, myMessage, statusCode = 200) {
+	res.writeHead(statusCode, {'Content-Type': 'text/plain; charset=UTF-8'});
+	res.end(myMessage); 
 }
 
+const getStatic = function(res, path) {
+	res.writeHead(200, {
+		'Content-Type': 'text/html'
+	});
+	readable = fs.createReadStream('.' + path);
+	readable.pipe(res)
+}
+
+const notFound = function(res, path)  {
+	res.writeHead(404, {'Content-Type' : 'text/plain'});
+	res.end('Requested path not found: ' + path); 
+}
+
+/* Listener **************************************************************** */
 const requestListener = function(req, res) {
 	switch(req.url) {
-		case '/nueva_nota.html': {
-			entregar_archivo('nueva_nota.html', res); 
+		case '/': 
+			dummyResponse(res, "Ya estás aquí; ahora, ¿qué quieres hacer?"); 
 			break; 
-		}
-		default: {
-			// Dummy
-			res.writeHead(200); 
-			res.end('<html><p>Respuesta dummy.</p><p>La URL que pediste es: ' + req.url + '</p><p>Gracias por participar.</p></html>')
-		}
+		case '/nueva_nota.html': 
+			getStatic(res, req.url); 			
+			break; 
+		default: 
+			notFound(res, req.url); 
+			break; 					
 	}
 	
 }	
 
-const servidor = http.createServer(requestListener); 
+/* Inicializar ************************************************************** */
+var servidor = http.createServer(requestListener); 
+servidor.listen(SRV_PUERTO); 
+console.log("Ola ke ase, servidor escuchando en puerto " + SRV_PUERTO); 
 
-servidor.listen(SRV_PUERTO, SRV_HOST, () => {
-	console.log("Ola ke ase, servidor corriendo"); 
-})
 
